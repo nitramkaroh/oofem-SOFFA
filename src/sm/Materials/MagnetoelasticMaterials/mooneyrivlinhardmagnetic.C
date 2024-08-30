d445628e66cd950be5fbb6bdd0983fe8aef8af8a
Ondřej Faltus <ondrej.faltus@cvut.cz>
2024-08-30 15:29:49 +0200

    Implementing material functions for multiphysics simulations, as new mode 5 of MooneyRivlinHardMagnetic

--- src/sm/Materials/MagnetoelasticMaterials/mooneyrivlinhardmagnetic.C
+++ src/sm/Materials/MagnetoelasticMaterials/mooneyrivlinhardmagnetic.C
@@ -39,6 +39,7 @@
 #include "mathfem.h"
 #include "domain.h"
 #include "function.h"
+#include "tensor/tensor3.h"
 
 
 
@@ -58,16 +59,19 @@ MooneyRivlinHardMagnetic::giveFirstPKStressVector_3d( const FloatArrayF<9> &vF, 
     FloatArrayF<9> vP_me;
     switch ( materialMode ) {
     case 1:
-        vP_me = giveFirstPKStressVector_3d_consistent( vF, gp, tStep );
+        vP_me = computeFirstPKStressVector_3d_consistent( vF, gp, tStep );
         break;
     case 2:
-        vP_me = giveFirstPKStressVector_3d_mit( vF, gp, tStep );
+        vP_me = computeFirstPKStressVector_3d_mit( vF, gp, tStep );
         break;
     case 3:
-        vP_me = giveFirstPKStressVector_3d_consistentfix( vF, gp, tStep );
+        vP_me = computeFirstPKStressVector_3d_consistentfix( vF, gp, tStep );
         break;
     case 4:
-        vP_me = giveFirstPKStressVector_3d_ogdenpullback( vF, gp, tStep );
+        vP_me = computeFirstPKStressVector_3d_ogdenpullback( vF, gp, tStep );
+        break;
+    case 5:
+        vP_me = computeFirstPKStressVector_3d_multiphysics( vF, gp, tStep );
         break;
     default:
         OOFEM_ERROR( "Unknown hard magnetic material mode %i", materialMode );
@@ -85,8 +89,6 @@ MooneyRivlinHardMagnetic::giveFirstPKStressVector_3d( const FloatArrayF<9> &vF, 
     return vP;
 }
 
-
-
 FloatMatrixF< 9, 9 >
 MooneyRivlinHardMagnetic::give3dMaterialStiffnessMatrix_dPdF( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
 // returns the 9x9 tangent stiffness matrix - dP/dF
@@ -98,16 +100,19 @@ MooneyRivlinHardMagnetic::give3dMaterialStiffnessMatrix_dPdF( MatResponseMode ma
     FloatMatrixF<9,9> vD_me;
     switch ( materialMode ) {
     case 1:
-        vD_me = give3dMaterialStiffnessMatrix_dPdF_consistent( matResponseMode, gp, tStep );
+        vD_me = compute3dMaterialStiffnessMatrix_dPdF_consistent( matResponseMode, gp, tStep );
         break;
     case 2:
-        vD_me = give3dMaterialStiffnessMatrix_dPdF_mit( matResponseMode, gp, tStep );
+        vD_me = compute3dMaterialStiffnessMatrix_dPdF_mit( matResponseMode, gp, tStep );
         break;
     case 3:
-        vD_me = give3dMaterialStiffnessMatrix_dPdF_consistentfix( matResponseMode, gp, tStep );
+        vD_me = compute3dMaterialStiffnessMatrix_dPdF_consistentfix( matResponseMode, gp, tStep );
         break;
     case 4:
-        vD_me = give3dMaterialStiffnessMatrix_dPdF_ogdenpullback( matResponseMode, gp, tStep );
+        vD_me = compute3dMaterialStiffnessMatrix_dPdF_ogdenpullback( matResponseMode, gp, tStep );
+        break;
+    case 5:
+        vD_me = compute3dMaterialStiffnessMatrix_dPdF_multiphysics( matResponseMode, gp, tStep );
         break;
     default:
         OOFEM_ERROR( "Unknown hard magnetic material mode %i", materialMode );
@@ -120,12 +125,38 @@ MooneyRivlinHardMagnetic::give3dMaterialStiffnessMatrix_dPdF( MatResponseMode ma
     return vD;
 }
 
+FloatArrayF<3> MooneyRivlinHardMagnetic::giveMagneticInduction_3d( const FloatArrayF<3> &vH, GaussPoint *gp, TimeStep *tStep ) const
+{
+    if ( materialMode < 5 ) {
+        OOFEM_ERROR( "Material not in multiphysics mode." );
+    }
+
+    return computeMagneticInduction_3d_multiphysics( vH, gp, tStep );
+}
+
+FloatMatrixF<3, 9> MooneyRivlinHardMagnetic::give3dMagnetoelasticCouplingTensor_dBdF( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
+{
+    if ( materialMode < 5 ) {
+        OOFEM_ERROR( "Material not in multiphysics mode." );
+    }
+
+    return compute3dMagnetoelasticCouplingTensor_dBdF_multiphysics( matResponseMode, gp, tStep );
+}
+
+FloatMatrixF<3, 3> MooneyRivlinHardMagnetic::give3dPermeabilityMatrix_dBdH( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
+{
+    if ( materialMode < 5 ) {
+        OOFEM_ERROR( "Material not in multiphysics mode." );
+    }
+
+    return compute3dPermeabilityMatrix_dBdH_multiphysics( matResponseMode, gp, tStep );
+}
 
 void MooneyRivlinHardMagnetic::initializeFrom( InputRecord &ir )
 {   
     MooneyRivlinCompressibleMaterial::initializeFrom( ir );
 
-    FloatArray B_app_temp, B_res_temp;
+    FloatArray B_app_temp, B_res_temp, H_temp;
 
     //default values
     mu_0 = BASE_VACUUM_PERMEABILITY_MU_0;
@@ -133,6 +164,7 @@ void MooneyRivlinHardMagnetic::initializeFrom( InputRecord &ir )
 
     IR_GIVE_FIELD( ir, B_app_temp, _IFT_MooneyRivlinHardMagnetic_B_app );
     IR_GIVE_FIELD( ir, B_res_temp, _IFT_MooneyRivlinHardMagnetic_B_res );
+    IR_GIVE_FIELD( ir, H_temp, _IFT_MooneyRivlinHardMagnetic_H );
     IR_GIVE_OPTIONAL_FIELD( ir, mu_0, _IFT_MooneyRivlinHardMagnetic_mu_0 );
     IR_GIVE_FIELD( ir, ltf_index, _IFT_MooneyRivlinHardMagnetic_ltf );
     IR_GIVE_OPTIONAL_FIELD( ir, materialMode, _IFT_MooneyRivlinHardMagnetic_mode );
@@ -142,9 +174,10 @@ void MooneyRivlinHardMagnetic::initializeFrom( InputRecord &ir )
 
     B_app = FloatArrayF<3>( B_app_temp );
     B_res = FloatArrayF<3>( B_res_temp );
+    H = FloatArrayF<3>( H_temp );
 }
 
-FloatArrayF<9> MooneyRivlinHardMagnetic::giveFirstPKStressVector_3d_consistent( const FloatArrayF<9> &vF, GaussPoint *gp, TimeStep *tStep ) const
+FloatArrayF<9> MooneyRivlinHardMagnetic::computeFirstPKStressVector_3d_consistent( const FloatArrayF<9> &vF, GaussPoint *gp, TimeStep *tStep ) const
 {
 
     double load_level            = this->giveDomain()->giveFunction( ltf_index )->evaluateAtTime( tStep->giveIntrinsicTime() );
@@ -169,7 +202,7 @@ FloatArrayF<9> MooneyRivlinHardMagnetic::giveFirstPKStressVector_3d_consistent( 
     return vP_me;
 }
 
-FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::give3dMaterialStiffnessMatrix_dPdF_consistent( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
+FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::compute3dMaterialStiffnessMatrix_dPdF_consistent( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
 {
     StructuralMaterialStatus *status = static_cast<StructuralMaterialStatus *>( this->giveStatus( gp ) );
     FloatArrayF<9> vF( status->giveTempFVector() );
@@ -203,7 +236,7 @@ FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::give3dMaterialStiffnessMatrix_dPdF_
     return vD_me;
 }
 
-FloatArrayF<9> MooneyRivlinHardMagnetic::giveFirstPKStressVector_3d_mit( const FloatArrayF<9> &vF, GaussPoint *gp, TimeStep *tStep ) const
+FloatArrayF<9> MooneyRivlinHardMagnetic::computeFirstPKStressVector_3d_mit( const FloatArrayF<9> &vF, GaussPoint *gp, TimeStep *tStep ) const
 {
 
     double load_level = this->giveDomain()->giveFunction( ltf_index )->evaluateAtTime( tStep->giveIntrinsicTime() );
@@ -219,12 +252,12 @@ FloatArrayF<9> MooneyRivlinHardMagnetic::giveFirstPKStressVector_3d_mit( const F
     return vP_me;
 }
 
-FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::give3dMaterialStiffnessMatrix_dPdF_mit( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
+FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::compute3dMaterialStiffnessMatrix_dPdF_mit( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
 {
     return FloatMatrixF<9, 9>();
 }
 
-FloatArrayF<9> MooneyRivlinHardMagnetic::giveFirstPKStressVector_3d_consistentfix( const FloatArrayF<9> &vF, GaussPoint *gp, TimeStep *tStep ) const
+FloatArrayF<9> MooneyRivlinHardMagnetic::computeFirstPKStressVector_3d_consistentfix( const FloatArrayF<9> &vF, GaussPoint *gp, TimeStep *tStep ) const
 {
 
     double load_level = this->giveDomain()->giveFunction( ltf_index )->evaluateAtTime( tStep->giveIntrinsicTime() );
@@ -243,7 +276,7 @@ FloatArrayF<9> MooneyRivlinHardMagnetic::giveFirstPKStressVector_3d_consistentfi
     return vP_me;
 }
 
-FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::give3dMaterialStiffnessMatrix_dPdF_consistentfix( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
+FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::compute3dMaterialStiffnessMatrix_dPdF_consistentfix( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
 {
     StructuralMaterialStatus *status = static_cast<StructuralMaterialStatus *>( this->giveStatus( gp ) );
     FloatArrayF<9> vF( status->giveTempFVector() );
@@ -267,7 +300,7 @@ FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::give3dMaterialStiffnessMatrix_dPdF_
     return vD_me;
 }
 
-FloatArrayF<9> MooneyRivlinHardMagnetic::giveFirstPKStressVector_3d_ogdenpullback( const FloatArrayF<9> &vF, GaussPoint *gp, TimeStep *tStep ) const
+FloatArrayF<9> MooneyRivlinHardMagnetic::computeFirstPKStressVector_3d_ogdenpullback( const FloatArrayF<9> &vF, GaussPoint *gp, TimeStep *tStep ) const
 {
 
     double load_level = this->giveDomain()->giveFunction( ltf_index )->evaluateAtTime( tStep->giveIntrinsicTime() );
@@ -287,7 +320,7 @@ FloatArrayF<9> MooneyRivlinHardMagnetic::giveFirstPKStressVector_3d_ogdenpullbac
     return vP_me;
 }
 
-FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::give3dMaterialStiffnessMatrix_dPdF_ogdenpullback( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
+FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::compute3dMaterialStiffnessMatrix_dPdF_ogdenpullback( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
 {
     StructuralMaterialStatus *status = static_cast<StructuralMaterialStatus *>( this->giveStatus( gp ) );
     FloatArrayF<9> vF( status->giveTempFVector() );
@@ -315,6 +348,118 @@ FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::give3dMaterialStiffnessMatrix_dPdF_
     auto vD_me = D_me.to_voigt_form();
 
     return vD_me;
+}
+
+//--------------------------------------------------------------------------- Multiphysics ----------------------------------------------------------
+
+FloatArrayF<9> MooneyRivlinHardMagnetic::computeFirstPKStressVector_3d_multiphysics( const FloatArrayF<9> &vF, GaussPoint *gp, TimeStep *tStep ) const
+{
+    MagnetoelasticMaterialStatus *status = static_cast<MagnetoelasticMaterialStatus *>( this->giveStatus( gp ) );
+    FloatArrayF<3> vH( status->giveTempHVector() );
+
+    FloatArrayF<3> vQ = vH + B_res*(1/mu_0); //Q = (H+M)
+
+    Tensor1_3d Q( vQ );
+    Tensor2_3d F( vF ), P_me;
+    auto [J, cofF] = F.compute_determinant_and_cofactor();
+    auto Fcross    = F.compute_tensor_cross_product();
+
+    P_me( i_3, j_3 ) = -( mu_0 / J ) * Fcross( i_3, j_3, m_3, n_3 ) * cofF( m_3, q_3 ) * Q( q_3 ) * Q( n_3 )
+                       + (mu_0 / (2*J*J)) * cofF(m_3, n_3) * Q(n_3) * cofF(m_3, k_3) * Q(k_3) * cofF(i_3, j_3);
+
+    return P_me.to_voigt_form();
+}
+
+FloatMatrixF<9, 9> MooneyRivlinHardMagnetic::compute3dMaterialStiffnessMatrix_dPdF_multiphysics( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
+{
+    MagnetoelasticMaterialStatus *status = static_cast<MagnetoelasticMaterialStatus *>( this->giveStatus( gp ) );
+    FloatArrayF<3> vH( status->giveTempHVector() );
+    FloatArrayF<9> vF( status->giveTempFVector() );
+
+    FloatArrayF<3> vQ = vH + B_res*(1/mu_0); //Q = (H+M)
+
+    Tensor1_3d Q( vQ );
+    Tensor2_3d F( vF ), P_me, GQQ;
+    auto [J, cofF] = F.compute_determinant_and_cofactor();
+    auto Fcross    = F.compute_tensor_cross_product();
+    GQQ( i_3, j_3 ) = cofF( i_3, k_3 ) * Q( k_3 ) * Q( j_3 );
+    auto GQQcross   = GQQ.compute_tensor_cross_product();
+
+    Tensor4_3d D_me;
+    
+    D_me( i_3, j_3, k_3, l_3 ) = (-mu_0/J)*(GQQcross(i_3, j_3, k_3, l_3) + ( Fcross( i_3, j_3, m_3, n_3 ) * Q(n_3) ) * ( Fcross( m_3, q_3, k_3, l_3 ) * Q( q_3 ) ))
+                               + ( mu_0 / ( 2 * J * J ) ) * ( cofF( m_3, n_3 ) * Q( n_3 ) * cofF( m_3, k_3 ) * Q( k_3 ) * Fcross( i_3, j_3, k_3, l_3 ) )
+                               - ( mu_0 / ( J * J * J ) ) * (cofF(m_3, n_3) * Q(n_3) * cofF(m_3, k_3) * Q(k_3) * cofF(i_3, j_3) * cofF(k_3, l_3)
+                               + ( mu_0 / (  J * J ) ) * cofF(i_3, j_3) * (Fcross(k_3, l_3, m_3, n_3) * cofF(m_3,q_3) * Q(q_3) * Q(n_3)));
+
+    return D_me.to_voigt_form();
+
+}
+
+FloatArrayF<3> MooneyRivlinHardMagnetic::computeMagneticInduction_3d_multiphysics( const FloatArrayF<3> &vH, GaussPoint *gp, TimeStep *tStep ) const
+{
+    MagnetoelasticMaterialStatus *status = static_cast<MagnetoelasticMaterialStatus *>( this->giveStatus( gp ) );
+    FloatArrayF<9> vF( status->giveTempFVector() );
+
+    FloatArrayF<3> vQ = vH + B_res*(1/mu_0); //Q = (H+M)
+
+    Tensor1_3d Q( vQ ), B_me;
+    Tensor2_3d F( vF );
+    auto [J, cofF] = F.compute_determinant_and_cofactor();
+    
+    B_me( i_3 ) = mu_0 / J * cofF( j_3, i_3 ) * cofF( j_3, k_3 ) * Q( k_3 );
+
+    return B_me.to_voigt_form();
+}
+
+FloatMatrixF<3, 9> MooneyRivlinHardMagnetic::compute3dMagnetoelasticCouplingTensor_dBdF_multiphysics( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
+{
+    MagnetoelasticMaterialStatus *status = static_cast<MagnetoelasticMaterialStatus *>( this->giveStatus( gp ) );
+    FloatArrayF<3> vH( status->giveTempHVector() );
+    FloatArrayF<9> vF( status->giveTempFVector() );
+
+    FloatArrayF<3> vQ = vH + B_res*(1/mu_0); //Q = (H+M)
+
+    Tensor1_3d Q( vQ );
+    Tensor2_3d F( vF ), P_me, delta(1., 0., 0., 0., 1., 0., 0., 0., 1. );
+    Tensor3_3d A_me;
+    auto [J, cofF] = F.compute_determinant_and_cofactor();
+    auto Fcross    = F.compute_tensor_cross_product();
+    
+    A_me( i_3, k_3, l_3 ) = mu_0 / J * ( delta( i_3, n_3 ) * cofF( m_3, q_3 ) * Q( q_3 ) + G( m_3, i_3 ) * Q( n_3 ) ) * Fcross( m_3, n_3, k_3, l_3 )
+                          - mu_0 / ( J * J ) * ( cofF( p_3, i_3 ) * cofF( p_3, q_3 ) * Q( q_3 ) * G( k_3, l_3 ) );
+
+    return A_me.to_voigt_form_3x9();
+}
+
+FloatMatrixF<3, 3> MooneyRivlinHardMagnetic::compute3dPermeabilityMatrix_dBdH_multiphysics( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const
+{
+    MagnetoelasticMaterialStatus *status = static_cast<MagnetoelasticMaterialStatus *>( this->giveStatus( gp ) );
+    FloatArrayF<9> vF( status->giveTempFVector() );
+    //independent of H, actually
+
+    Tensor2_3d F( vF ), C_me;
+    auto [J, cofF] = F.compute_determinant_and_cofactor();
+
+    C_me( i_3, k_3 ) = mu_0 / J * cofF( j_3, i_3 ) * cofF( j_3, k_3 );
+
+    return C_me.to_voigt_form();
+}
+
+// TODO remove, temporary debug
+int
+MooneyRivlinHardMagnetic :: giveIPValue(FloatArray &answer, GaussPoint *gp, InternalStateType type, TimeStep *tStep)
+{
+    if ( type == IST_CrackVector ) {
+        answer = B_app;
+        return 1;
+    } else if ( type == IST_2ndCrackVector ) {
+        answer = H;
+        return 1;
+    }
+    {
+        return MooneyRivlinCompressibleMaterial::giveIPValue( answer, gp, type, tStep );
+  }
 }
 
 } // end namespace oofem
--- src/sm/Materials/MagnetoelasticMaterials/mooneyrivlinhardmagnetic.h
+++ src/sm/Materials/MagnetoelasticMaterials/mooneyrivlinhardmagnetic.h
@@ -45,9 +45,10 @@
 #define _IFT_MooneyRivlinHardMagnetic_Name "mooneyrivlinhardmagnetic"
 #define _IFT_MooneyRivlinHardMagnetic_B_app "b_app" //applied magnetic field
 #define _IFT_MooneyRivlinHardMagnetic_B_res "b_res" //residual magnetic field in material
+#define _IFT_MooneyRivlinHardMagnetic_H "h" //magnetic field in material
 #define _IFT_MooneyRivlinHardMagnetic_mu_0 "mu_0" //magnetic permeability of free space (optional, default value is 4*pi*10^7)
 #define _IFT_MooneyRivlinHardMagnetic_ltf "ltf" //load time function for applied field
-#define _IFT_MooneyRivlinHardMagnetic_mode "mode" //mode of magnetic energy 1 = consistent (default), 2 = MIT
+#define _IFT_MooneyRivlinHardMagnetic_mode "mode" //mode of magnetic energy 1 = consistent (default), 2 = MIT, 3 - fix, 4 - with Ogden pullback, 5 - multiphysics
 #define _IFT_MooneyRivlinHardMagnetic_referenceB "referenceb" //whether b_app is given in reference configuration, default false
 //@}
 
@@ -62,12 +63,13 @@ namespace oofem {
  *
  * @note
  */
-class MooneyRivlinHardMagnetic : public MooneyRivlinCompressibleMaterial
+class MooneyRivlinHardMagnetic : public MooneyRivlinCompressibleMaterial, public MagnetoelasticMaterial
 {
 protected:
     // Material parameters
     FloatArrayF<3> B_app;
     FloatArrayF<3> B_res;
+    FloatArrayF<3> H;
     double mu_0;
     int ltf_index, materialMode;
     bool referenceB;
@@ -78,34 +80,45 @@ public:
 
     void initializeFrom(InputRecord &ir) override;
 
-    FloatMatrixF< 9, 9 >give3dMaterialStiffnessMatrix_dPdF(MatResponseMode matResponseMode,
-                                                           GaussPoint *gp,
-                                                           TimeStep *tStep) const override;
-
+    //hyperelasticity
+    FloatMatrixF< 9, 9 >give3dMaterialStiffnessMatrix_dPdF(MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep) const override;
     FloatArrayF< 9 >giveFirstPKStressVector_3d(const FloatArrayF< 9 > &vF, GaussPoint *gp, TimeStep *tStep) const override;
+
+    //magnetoelastic multiphysics
+    FloatArrayF<3> giveMagneticInduction_3d( const FloatArrayF<3> &vH, GaussPoint *gp, TimeStep *tStep ) const override;
+    FloatMatrixF<3, 9> give3dMagnetoelasticCouplingTensor_dBdF(MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep) const override;
+    FloatMatrixF<3, 3> give3dPermeabilityMatrix_dBdH( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const override;
 
     const char *giveInputRecordName() const override { return _IFT_MooneyRivlinHardMagnetic_Name; }
     const char *giveClassName() const override { return "MooneyRivlinHardMagnetic"; }
 
 private:
-      FloatArrayF< 9 > giveFirstPKStressVector_3d_consistent(const FloatArrayF< 9 > &vF, GaussPoint *gp, TimeStep *tStep) const;
-      FloatMatrixF< 9, 9 > give3dMaterialStiffnessMatrix_dPdF_consistent(MatResponseMode matResponseMode,
+      FloatArrayF< 9 > computeFirstPKStressVector_3d_consistent(const FloatArrayF< 9 > &vF, GaussPoint *gp, TimeStep *tStep) const;
+      FloatMatrixF< 9, 9 > compute3dMaterialStiffnessMatrix_dPdF_consistent(MatResponseMode matResponseMode,
                                                            GaussPoint *gp,
                                                            TimeStep *tStep) const;
 
-      FloatArrayF< 9 > giveFirstPKStressVector_3d_mit(const FloatArrayF< 9 > &vF, GaussPoint *gp, TimeStep *tStep) const;
-      FloatMatrixF< 9, 9 > give3dMaterialStiffnessMatrix_dPdF_mit(MatResponseMode matResponseMode,
+      FloatArrayF< 9 > computeFirstPKStressVector_3d_mit(const FloatArrayF< 9 > &vF, GaussPoint *gp, TimeStep *tStep) const;
+      FloatMatrixF< 9, 9 > compute3dMaterialStiffnessMatrix_dPdF_mit(MatResponseMode matResponseMode,
                                                            GaussPoint *gp,
                                                            TimeStep *tStep) const;
 
-      FloatArrayF< 9 > giveFirstPKStressVector_3d_consistentfix(const FloatArrayF< 9 > &vF, GaussPoint *gp, TimeStep *tStep) const;
-      FloatMatrixF< 9, 9 > give3dMaterialStiffnessMatrix_dPdF_consistentfix(MatResponseMode matResponseMode,
+      FloatArrayF< 9 > computeFirstPKStressVector_3d_consistentfix(const FloatArrayF< 9 > &vF, GaussPoint *gp, TimeStep *tStep) const;
+      FloatMatrixF< 9, 9 > compute3dMaterialStiffnessMatrix_dPdF_consistentfix(MatResponseMode matResponseMode,
                                                            GaussPoint *gp,
                                                            TimeStep *tStep) const;
 
-      FloatArrayF< 9 > giveFirstPKStressVector_3d_ogdenpullback(const FloatArrayF< 9 > &vF, GaussPoint *gp, TimeStep *tStep) const;
-      FloatMatrixF< 9, 9 > give3dMaterialStiffnessMatrix_dPdF_ogdenpullback(MatResponseMode matResponseMode,
-                                                           GaussPoint *gp,
-                                                           TimeStep *tStep) const;
+      FloatArrayF< 9 > computeFirstPKStressVector_3d_ogdenpullback(const FloatArrayF< 9 > &vF, GaussPoint *gp, TimeStep *tStep) const;
+      FloatMatrixF< 9, 9 > compute3dMaterialStiffnessMatrix_dPdF_ogdenpullback(MatResponseMode matResponseMode,
+                                                           GaussPoint *gp, TimeStep *tStep ) const;
+
+      FloatArrayF< 9 > computeFirstPKStressVector_3d_multiphysics(const FloatArrayF< 9 > &vF, GaussPoint *gp, TimeStep *tStep) const;
+      FloatMatrixF<9, 9> compute3dMaterialStiffnessMatrix_dPdF_multiphysics( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const;
+      FloatArrayF<3> computeMagneticInduction_3d_multiphysics( const FloatArrayF<3> &vH, GaussPoint *gp, TimeStep *tStep ) const;
+      FloatMatrixF<3, 9> compute3dMagnetoelasticCouplingTensor_dBdF_multiphysics( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const;
+      FloatMatrixF<3, 3> compute3dPermeabilityMatrix_dBdH_multiphysics( MatResponseMode matResponseMode, GaussPoint *gp, TimeStep *tStep ) const;
+
+      //todo remove this \/
+      int giveIPValue( FloatArray &answer, GaussPoint *gp, InternalStateType type, TimeStep *tStep ) override;
 };
 } // end namespace oofem
