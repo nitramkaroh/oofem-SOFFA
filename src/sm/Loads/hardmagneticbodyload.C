e9e51222616b2db9e8e6f71d0a0905895151c1d9
Ondřej Faltus <ondrej.faltus@cvut.cz>
2024-10-10 12:08:12 +0200

    Adding HardMagneticBodyLoad. Contents for now copied from HMBC.

---
+++ src/sm/Loads/hardmagneticbodyload.C
@@ -0,0 +1,273 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2020   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "hardmagneticbodyload.h"
+#include "classfactory.h"
+#include "MagnetoelasticMaterials/mooneyrivlinhardmagnetic.h"
+#include "sparsemtrx.h"
+#include "feinterpol.h"
+#include "magneticloadinterface.h"
+#include "interfacetype.h"
+#include "mathfem.h"
+#include "function.h"
+#include "tensor/tensor3.h"
+#include "nlstructuralelement.h"
+#include "feinterpol3d.h"
+
+namespace oofem {
+REGISTER_BoundaryCondition( HardMagneticBodyLoad );
+
+    void HardMagneticBodyLoad::initializeFrom( InputRecord &ir )
+    {
+        ActiveBoundaryCondition::initializeFrom( ir );
+
+	    FloatArray b_temp, m_temp;
+        IR_GIVE_FIELD( ir, b_temp, _IFT_HardMagneticBodyLoad_b_ext );
+	    b_app = FloatArrayF< 3 >( b_temp );
+
+        IR_GIVE_FIELD( ir, m_temp, _IFT_HardMagneticBodyLoad_mjump );
+        m_jump = FloatArrayF<3>( m_temp );
+
+        IR_GIVE_FIELD( ir, ltf_index, _IFT_HardMagneticBodyLoad_ltf );
+
+        mu0 = BASE_VACUUM_PERMEABILITY_MU_0;
+        IR_GIVE_OPTIONAL_FIELD( ir, mu0, _IFT_HardMagneticBodyLoad_mu_0 );
+    }
+
+    void HardMagneticBodyLoad::assemble( SparseMtrx &answer, TimeStep *tStep,
+        CharType type , const UnknownNumberingScheme& r_s , const UnknownNumberingScheme& c_s , double scale , void* lock) {
+        
+        if ( type != TangentStiffnessMatrix ) {
+            return;
+        }
+
+        FloatMatrix Ke;
+        IntArray r_loc, c_loc, bNodes, dofIDs;
+
+        dofIDs = domain->giveDefaultNodeDofIDArry();
+
+        Set *set                   = this->giveDomain()->giveSet( this->set );
+        const IntArray &boundaries = set->giveBoundaryList();
+
+        for ( int pos = 1; pos <= boundaries.giveSize() / 2; ++pos ) {
+            Element *e   = this->giveDomain()->giveElement( boundaries.at( pos * 2 - 1 ) );
+            int boundary = boundaries.at( pos * 2 );
+            if ( e->giveSpatialDimension() == 2 ) {
+                bNodes = e->giveInterpolation()->boundaryEdgeGiveNodes( boundary );
+            } else {
+                bNodes = e->giveInterpolation()->boundarySurfaceGiveNodes( boundary );
+            }
+
+            e->giveBoundaryLocationArray( r_loc, bNodes, dofIDs /* this->dofs*/, r_s );
+            e->giveBoundaryLocationArray( c_loc, bNodes, dofIDs /* this->dofs*/, c_s );
+            this->computeTangentFromElement( Ke, e, boundary, tStep );
+
+            // debug
+            // compute numerical tangent
+            FloatMatrix Ke_num;
+            double pert = 1.e-6;
+            this->computeNumericalTangentFromElement( Ke_num, e, boundary, tStep, pert );
+
+            //symmetry check
+            FloatMatrix KeT, Ke_numT, errorKe, errorKe_num;
+            KeT.beTranspositionOf( Ke );
+            Ke_numT.beTranspositionOf( Ke_num );
+            errorKe = KeT;
+            errorKe_num = Ke_numT;
+            errorKe.add( -1. * Ke );
+            errorKe_num.add( -1. * Ke_num );
+
+            if ( pos < 0 ) {
+                
+                OOFEM_LOG_INFO( "Debugging surface %i\n", boundary );
+                OOFEM_LOG_INFO( "-------Analytical stiffness-----------\n" );
+                Ke.printYourself();
+                OOFEM_LOG_INFO( "-------Numerical stiffness------------\n" );
+                Ke_num.printYourself();
+            }
+
+            answer.assemble( r_loc, c_loc, Ke_num );
+        }
+    }    
+    
+    void HardMagneticBodyLoad::assembleVector(FloatArray& answer , TimeStep* tStep ,
+        CharType type , ValueModeType mode ,
+        const UnknownNumberingScheme& s , FloatArray* eNorms, void* lock ) {
+
+        if ( type != ExternalForcesVector ) {
+            return;
+        }
+
+        FloatArray fe;
+        IntArray loc, masterdofids, bNodes, dofIDs;
+        dofIDs = domain->giveDefaultNodeDofIDArry();
+
+        Set *set                   = this->giveDomain()->giveSet( this->set );
+        const IntArray &boundaries = set->giveBoundaryList();
+        for ( int pos = 1; pos <= boundaries.giveSize() / 2; ++pos ) {
+            Element *e   = this->giveDomain()->giveElement( boundaries.at( pos * 2 - 1 ) );
+            int boundary = boundaries.at( pos * 2 );
+            if ( e->giveSpatialDimension() == 2 ) {
+                bNodes = e->giveInterpolation()->boundaryEdgeGiveNodes( boundary );
+            } else {
+                bNodes = e->giveInterpolation()->boundarySurfaceGiveNodes( boundary );
+            }
+            e->giveBoundaryLocationArray( loc, bNodes, dofIDs /* this->dofs*/, s, &masterdofids );
+            this->computeLoadVectorFromElement( fe, e, boundary, tStep, mode );
+            answer.assemble( fe, loc );
+            if ( eNorms ) {
+                eNorms->assembleSquared( fe, masterdofids );
+            }
+        }
+
+    }
+
+    void HardMagneticBodyLoad::giveLocationArrays( std::vector<IntArray> &rows, std::vector<IntArray> &cols, CharType type, const UnknownNumberingScheme &r_s, const UnknownNumberingScheme &c_s )
+    {
+        if ( type != TangentStiffnessMatrix ) {
+            return;
+        }
+
+        Set *set                   = this->giveDomain()->giveSet( this->set );
+        const IntArray &boundaries = set->giveBoundaryList();
+        IntArray bNodes, dofIDs;
+        dofIDs = domain->giveDefaultNodeDofIDArry();
+
+        rows.resize( boundaries.giveSize() / 2 );
+        cols.resize( boundaries.giveSize() / 2 );
+
+        for ( int pos = 1; pos <= boundaries.giveSize() / 2; ++pos ) {
+            Element *e   = this->giveDomain()->giveElement( boundaries.at( pos * 2 - 1 ) );
+            int boundary = boundaries.at( pos * 2 );
+
+            bNodes = e->giveInterpolation()->boundaryGiveNodes( boundary );
+
+            e->giveBoundaryLocationArray( rows[pos], bNodes, dofIDs /* this->dofs*/, r_s );
+            e->giveBoundaryLocationArray( cols[pos], bNodes, dofIDs /* this->dofs*/, c_s );
+        }
+    }
+
+    void HardMagneticBodyLoad::computeLoadVectorFromElement( FloatArray &answer, Element *e, int iSurf, TimeStep *tStep, ValueModeType mode )
+    {
+	
+        
+    }
+
+    void HardMagneticBodyLoad::computeTangentFromElement( FloatMatrix &answer, Element *e, int iSurf, TimeStep *tStep )
+    {
+
+	    
+    }
+    
+
+
+    //----------------------------------------------------------------------DEBUG-------------------------------------------------------------------------------------
+
+    void HardMagneticBodyLoad::computeNumericalTangentFromElement( FloatMatrix &answer, Element *e, int side, TimeStep *tStep, double perturb )
+    {
+        // debugging, numerically computing stiffness
+        answer.resize( 12, 12 );
+
+        FloatArray perturbedForceFront, perturbedForceBack;
+        for ( int i = 1; i <= 12; i++ ) {
+
+            this->computePerturbedLoadVectorFromElement( perturbedForceFront, e, side, tStep, perturb, i );
+            this->computePerturbedLoadVectorFromElement( perturbedForceBack, e, side, tStep, -perturb, i );
+
+            for ( int j = 1; j <= 12; j++ ) {
+
+                answer.at( i, j ) = ( perturbedForceFront.at( j ) - perturbedForceBack.at( j ) ) / (2 * perturb);
+            }
+        }
+    }
+
+    void HardMagneticBodyLoad::computePerturbedLoadVectorFromElement( FloatArray &answer, Element *e, int iSurf, TimeStep *tStep, double perturb, int index )
+    {
+        // debugging, numerically perturbed load vector
+        IntArray bNodes = e->giveInterpolation()->boundarySurfaceGiveNodes( iSurf );
+
+        int ndof = 3 * bNodes.giveSize();
+        answer.resize( ndof );
+
+        //standard load vector computation... almost:
+        NLStructuralElement *nle           = dynamic_cast<NLStructuralElement *>( e );
+        FEInterpolation3d *interpolation = dynamic_cast<FEInterpolation3d *>( e->giveInterpolation() );
+
+        if ( nle == nullptr || interpolation == nullptr) {
+            OOFEM_ERROR( "Nonlinear elements required for magnetic BCs" );
+        }
+
+        if ( iSurf == -1 ) {
+            iSurf = 1;
+        }
+
+        double load_level = this->giveDomain()->giveFunction( ltf_index )->evaluateAtTime( tStep->giveIntrinsicTime() );
+
+
+        answer.clear();
+
+        int order  = 4;
+        auto iRule = nle->giveBoundarySurfaceIntegrationRule( order, iSurf );
+
+        Tensor1_3d maxwellStressCofFN;
+        Tensor2_3d F;
+        FloatArray vF, uPert;
+
+        for ( GaussPoint *gp : *iRule ) {
+            // compute normal vector
+            auto [dA, n] = interpolation->surfaceEvalUnitNormal( iSurf, gp->giveNaturalCoordinates(), FEIElementGeometryWrapper( e ) );
+            // compute surface N matirx
+            FloatMatrix N, B;
+            nle->computeSurfaceNMatrix( N, iSurf, gp->giveNaturalCoordinates() );
+            nle->computeBHmatrixAtBoundary( gp, B, iSurf );
+            // compute deformation gradient
+            // do it from perturbed displacements
+            nle->computeBoundaryVectorOf( bNodes, { D_u, D_v, D_w }, VM_Total, tStep, uPert );
+            uPert.at( index ) += perturb;
+            vF.beProductOf( B, uPert );
+            vF.at( 1 ) += 1.0;
+            vF.at( 2 ) += 1.0;
+            vF.at( 3 ) += 1.0;
+
+            // compute the force vector
+            auto F                    = Tensor2_3d( FloatArrayF<9>( vF ) );
+            auto Normal               = Tensor1_3d( FloatArrayF<3>( n ) );
+            auto [J, cofF]            = F.compute_determinant_and_cofactor();
+            //
+            answer.plusProduct( N, maxwellStressCofFN.to_voigt_form(), -gp->giveWeight() * dA );
+        }
+
+    }
+
+    }//end namespace oofem;
