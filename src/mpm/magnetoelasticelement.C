534816db08afaef9bc96f75970955be0abdf5fc0
drafon <0xdrafon@gmail.com>
2025-02-07 15:05:57 +0100

    Adding qq multiphys element

--- src/mpm/magnetoelasticelement.C
+++ src/mpm/magnetoelasticelement.C
@@ -76,7 +76,7 @@ class MagnetoElasticElement : public MPElement {
 
         if (type == MagnetoElasticity_GradGrad_dFluxdGrad) {
             int udofs = this->giveNumberOfUDofs();
-	    int phidofs = this->giveNumberOfPhiDofs(); 
+	        int phidofs = this->giveNumberOfPhiDofs(); 
             answer.resize(udofs+phidofs,udofs+phidofs);
             answer.zero();
             this->integrateTerm_dw (answer, MagnetoElasticity_GradGrad_Term(getU(),getU(), getPhi()), ir, tStep) ;
@@ -99,6 +99,82 @@ class MagnetoElasticElement : public MPElement {
     }
 
 };
+
+/**
+ * @brief 2D Magneto elatic element with quadratic interpolation of displacements and linear interpolation of magnetic potential
+ * 
+ */
+class MagnetoElasticQuad_qq : public MagnetoElasticElement {
+    protected:
+        const static FEInterpolation & phiInterpol;
+        const static FEInterpolation & uInterpol;
+        const static Variable& phi;
+        const static Variable& u;
+      
+    public:
+  MagnetoElasticQuad_qq(int n, Domain* d):  MagnetoElasticElement(n,d)
+    {
+        numberOfDofMans  = 8;
+        numberOfGaussPoints = 4;
+        this->computeGaussPoints();
+    }
+
+  int getNumberOfSurfaceDOFs() const override {return 0;}
+  int getNumberOfEdgeDOFs() const override {return 0;}
+  void getSurfaceLocalCodeNumbers(oofem::IntArray&, oofem::Variable::VariableQuantity) const override {;}
+
+  void getDofManLocalCodeNumbers (IntArray& answer, const Variable::VariableQuantity q, int num ) const  override {
+        /* dof ordering: u1 v1 phi1  u2 v2 phi2  u3 v3 phi3  u4 v4 phi4   u5 v5 u6 v6 */
+        if (q == Variable::VariableQuantity::Displacement) {
+          //answer={1,2, 4,5,  7,8, 10,11,  13,14  15,16, 17,18  19,20};
+	  int o = (num-1)*3+1;
+          answer={o, o+1};
+        } else if (q == Variable::VariableQuantity::MagneticPotential) {
+	    answer={num*3};
+        }
+    }
+    void getInternalDofManLocalCodeNumbers (IntArray& answer, const Variable::VariableQuantity q, int num ) const  override {
+        answer={};
+    }
+
+    void giveDofManDofIDMask(int inode, IntArray &answer) const override {
+    
+            answer = {1,2,55};
+    }
+    int giveNumberOfDofs() override { return 24; }
+    const char *giveInputRecordName() const override {return "MagnetoElasticQuad_qq";}
+    const char *giveClassName() const override { return "MagnetoElasticQuad_qq"; }
+
+    
+    const FEInterpolation& getGeometryInterpolation() const override {return this->uInterpol;}
+  
+    Element_Geometry_Type giveGeometryType() const override {
+        return EGT_quad_2;
+    }
+    void getEdgeLocalCodeNumbers(IntArray& answer, const Variable::VariableQuantity q) const override {}
+
+
+private:
+        virtual int  giveNumberOfUDofs() const override {return 16;} 
+        virtual int  giveNumberOfPhiDofs() const override {return 8;}
+        virtual const Variable& getU() const override {return u;}
+        virtual const Variable& getPhi() const override {return phi;}
+        void computeGaussPoints() override {
+            if ( integrationRulesArray.size() == 0 ) {
+                integrationRulesArray.resize( 1 );
+                integrationRulesArray [ 0 ] = std::make_unique<GaussIntegrationRule>(1, this);
+                integrationRulesArray [ 0 ]->SetUpPointsOnSquare(numberOfGaussPoints, _PlaneStrain);
+            }
+        }
+};
+
+  const FEInterpolation &  MagnetoElasticQuad_qq :: uInterpol = FEI2dQuadQuad(1,2);
+  const FEInterpolation &  MagnetoElasticQuad_qq :: phiInterpol = FEI2dQuadQuad(1,2);
+const Variable&   MagnetoElasticQuad_qq::phi = Variable(  MagnetoElasticQuad_qq::phiInterpol, Variable::VariableQuantity::MagneticPotential, Variable::VariableType::scalar, 1, NULL, {55});
+const Variable&   MagnetoElasticQuad_qq::u = Variable(MagnetoElasticQuad_qq::uInterpol, Variable::VariableQuantity::Displacement, Variable::VariableType::vector, 2, NULL, {1,2});
+
+#define _IFT_MagnetoElasticQuad_qq_Name "magnetoelasticquad_qq"
+REGISTER_Element(MagnetoElasticQuad_qq)
 
 
 
---
+++ src/sm/Loads/hardmagneticboundarycondition.h
@@ -0,0 +1,165 @@
+/*
+ *
+ *                 #####    #####   ######  ######  ###   ###
+ *               ##   ##  ##   ##  ##      ##      ## ### ##
+ *              ##   ##  ##   ##  ####    ####    ##  #  ##
+ *             ##   ##  ##   ##  ##      ##      ##     ##
+ *            ##   ##  ##   ##  ##      ##      ##     ##
+ *            #####    #####   ##      ######  ##     ##
+ *
+ *
+ *             OOFEM : Object Oriented Finite Element Code
+ *
+ *               Copyright (C) 1993 - 2020   Borek Patzak
+ *
+ *
+ *
+ *       Czech Technical University, Faculty of Civil Engineering,
+ *   Department of Structural Mechanics, 166 29 Prague, Czech Republic
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#pragma once
+
+#include "activebc.h"
+#include "element.h"
+#include "inputrecord.h"
+#include "tensor/tensor1.h"
+#include "tensor/tensor2.h"
+///@name Input fields for HardMagneticBoundaryCondition
+//@{
+#define _IFT_HardMagneticBoundaryCondition_Name "hardmagneticboundarycondition"
+#define _IFT_HardMagneticBoundaryCondition_mu_0 "mu_0"
+#define _IFT_HardMagneticBoundaryCondition_b_ext "b_ext"
+#define _IFT_HardMagneticBoundaryCondition_mjump "mjump"
+#define _IFT_HardMagneticBoundaryCondition_mode "mode" //1 = maxwell only, 2 = body load approach
+#define _IFT_HardMagneticBoundaryCondition_ltf "loadtimefunction" //load time function for applied field
+//@}
+
+namespace oofem {
+/**
+ * This class implements a hard magnetic boundary condition
+ *
+ * @author Ondrej Faltus
+ *
+ * @note
+ */
+    
+class HardMagneticBoundaryCondition : public ActiveBoundaryCondition {
+
+protected:
+
+    double mu0; //vacuum permeability
+    FloatArrayF<3> b_app, m_jump; //external magnetic field in free space, jump in magnetization
+  //FloatArrayF<9> sigma_star; //precomputed free space stress
+    Tensor2_3d maxwell_stress;
+    int ltf_index; //index of load time function for applied load
+    int bcMode; //mode of boundary condition implemented
+
+public:
+    /// <summary>
+    ///  Base Constructor
+    /// </summary>
+    /// <param name="n"></param>
+    /// <param name="d"></param>
+    HardMagneticBoundaryCondition( int n, Domain *d ) :
+        ActiveBoundaryCondition( n, d ) {}
+    /// Destructor.
+    virtual ~HardMagneticBoundaryCondition() {}
+
+
+    virtual void initializeFrom(InputRecord &ir) override;
+
+        /**
+     * Assembles B.C. contributions to specified matrix.
+     * @param[in,out] answer Matrix to assemble to.
+     * @param tStep Active time step.
+     * @param tStep Active time step.
+     * @param type Type of matrix to assemble.
+     * @param r_s Row numbering scheme.
+     * @param c_s Column numbering scheme.
+     * @param scale Scaling factor.
+     * @param omp_lock optional OMP lock to ensure correct update of answer
+     */
+    virtual void assemble( SparseMtrx &answer, TimeStep *tStep,
+        CharType type, const UnknownNumberingScheme &r_s, const UnknownNumberingScheme &c_s, double scale = 1.0, void *lock = nullptr ) override;
+
+    /**
+     * Assembles B.C. contributions to specified vector.
+     * @param[in,out] answer Vector to assemble to.
+     * @param tStep Active time step.
+     * @param type Type of matrix to assemble.
+     * @param mode Mode of value.
+     * @param s Numbering scheme.
+     * @param eNorms Norms for each dofid.
+     * @param omp_lock optional OMP lock to ensure correct update of answer
+     * @return Equivalent of the sum of the element norm (squared) of assembled vector.
+     */
+    virtual void assembleVector(FloatArray &answer, TimeStep *tStep,
+                                CharType type, ValueModeType mode,
+                                const UnknownNumberingScheme &s, FloatArray *eNorms=nullptr, void*lock=nullptr) override;
+
+    /**
+     * Gives a list of location arrays that will be assembled.
+     * This should only be used to construct zero structure in sparse matrices.
+     * The rows and columns location arrays returned in tuples (stored in vector),
+     * allowing to efficiently assemble and allocate off-diagonal blocks.
+     * The nonzero entries are assembled and allocated for entries at (rows[i], cols[i]) positions.
+     * @param rows List of location arrays for r_s.
+     * @param cols List of location arrays for c_s.
+     * @param type Type of matrix to assemble.
+     * @param r_s Row numbering scheme.
+     * @param c_s Column numbering scheme.
+     */
+    virtual void giveLocationArrays( std ::vector<IntArray> &rows, std ::vector<IntArray> &cols, CharType type,
+        const UnknownNumberingScheme &r_s, const UnknownNumberingScheme &c_s ) override;
+
+    const char *giveInputRecordName() const override { return _IFT_HardMagneticBoundaryCondition_Name; }
+    const char *giveClassName() const override { return "HardMagneticBoundaryCondition"; }
+
+protected:
+    /**
+     * Helper function for computing the contributions to the load vector.
+     */
+    void computeLoadVectorFromElement(FloatArray &answer, Element *e, int side, TimeStep *tStep, ValueModeType mode);
+    /**
+     * Helper function for computing the tangent (@f$ K = \frac{\mathrm{d}F}{\mathrm{d}u} @f$)
+     */
+    void computeTangentFromElement(FloatMatrix &answer, Element *e, int side, TimeStep *tStep);
+
+private:
+
+    // Maxwell stress approach
+    void computeElementLoadVectorContribution_maxwell( FloatArray &answer, Element * e, int iSurf, TimeStep *tStep );
+    void computeElementTangentContribution_maxwell( FloatMatrix &answer, Element * e, int iSurf, TimeStep *tStep );
+
+    // Body load approach
+    void computeElementLoadVectorContribution_bla( FloatArray &answer, Element * e, int iSurf, TimeStep *tStep );
+    void computeElementTangentContribution_bla( FloatMatrix &answer, Element * e, int iSurf, TimeStep *tStep );
+
+    // Maxwell 2 approach
+    void computeElementLoadVectorContribution_maxwell2( FloatArray &answer, Element * e, int iSurf, TimeStep *tStep );
+    void computeElementTangentContribution_maxwell2( FloatMatrix &answer, Element * e, int iSurf, TimeStep *tStep );
+
+    void evaluateFreeSpaceStress();
+
+    void computeNumericalTangentFromElement( FloatMatrix &answer, Element *e, int side, TimeStep *tStep, double perturb ); //for debugging
+    void computePerturbedLoadVectorFromElement( FloatArray &answer, Element *e, int side, TimeStep *tStep, double perturb, int index);
+    void computePerturbedLoadVectorFromElement_maxwell( FloatArray &answer, Element *e, int side, TimeStep *tStep, double perturb, int index);
+    void computePerturbedLoadVectorFromElement_bla( FloatArray &answer, Element *e, int side, TimeStep *tStep, double perturb, int index);
+    void computePerturbedLoadVectorFromElement_maxwell2( FloatArray &answer, Element *e, int side, TimeStep *tStep, double perturb, int index);
+};
+} // end namespace oofem
