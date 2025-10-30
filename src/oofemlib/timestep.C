87378d9542e37ae0b7f5e796e0f53cc0d124b974
nitram <nitramkaroh@seznam.cz>
2025-10-30 11:03:37 +0100

    Implementation of time step reduction

--- src/oofemlib/timestep.C
+++ src/oofemlib/timestep.C
@@ -161,6 +161,12 @@ bool TimeStep :: isTheCurrentTimeStep()
     return this == eModel->giveCurrentStep();
 }
 
+void TimeStep :: setTimeStepReductionFactor(double tsrf)
+{
+  this->eModel->giveCurrentMetaStep()->setTimeStepReductionFactor(tsrf);
+}
+
+
 
 void
 TimeStep :: saveContext(DataStream &stream)
